18d2cda424e961f250b283636021886a1a8c55ca
cielomaluquis-beep <cielomaluquis@gmail.com>
2026-07-01 18:40:38 -0500

    agregando el interface ipago.

--- ResidenciaAJ-2026/src/java/Interface/IPago.java
+++ ResidenciaAJ-2026/src/java/Interface/IPago.java
@@ -1,21 +1,11 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
- */
 package Interface;
 
 import Model.Pago;
 import java.util.List;
 
-/**
- *
- * @author cielo
- */
 public interface IPago {
-    List<Pago> lista();
-    int insert(Pago p);
-    boolean update(Pago p);
-    Pago searchById(int id);
-    boolean delete(int id);
-    
+    public List<Pago> listar();
+    public Pago buscar(int id);
+    public boolean insert(Pago pago);
+    public boolean anular(int id);
 }
