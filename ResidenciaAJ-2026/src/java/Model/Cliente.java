34d030351f526d12e1c4442ce4bb5b463cb1a642
cielomaluquis-beep <cielomaluquis@gmail.com>
2026-07-01 18:33:54 -0500

    agregando el model cliente.

---
+++ ResidenciaAJ-2026/src/java/Model/Cliente.java
@@ -0,0 +1,81 @@
+package Model;
+
+import java.sql.Date;
+
+public class Cliente {
+    private int id_cliente;
+    private String nombre;
+    private String ap_paterno;
+    private String ap_materno;
+    private String nro_documento;
+    private String telefono;
+    private String email;
+    private Date fecha_nac;
+
+    public Cliente() {
+    }
+
+    public int getId_cliente() {
+        return id_cliente;
+    }
+
+    public void setId_cliente(int id_cliente) {
+        this.id_cliente = id_cliente;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getAp_paterno() {
+        return ap_paterno;
+    }
+
+    public void setAp_paterno(String ap_paterno) {
+        this.ap_paterno = ap_paterno;
+    }
+
+    public String getAp_materno() {
+        return ap_materno;
+    }
+
+    public void setAp_materno(String ap_materno) {
+        this.ap_materno = ap_materno;
+    }
+
+    public String getNro_documento() {
+        return nro_documento;
+    }
+
+    public void setNro_documento(String nro_documento) {
+        this.nro_documento = nro_documento;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Date getFecha_nac() {
+        return fecha_nac;
+    }
+
+    public void setFecha_nac(Date fecha_nac) {
+        this.fecha_nac = fecha_nac;
+    }
+}
