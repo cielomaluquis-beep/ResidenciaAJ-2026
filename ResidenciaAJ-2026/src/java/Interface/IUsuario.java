9d80304fa1504222168db23281c411504f368a20
cielomaluquis-beep <cielomaluquis@gmail.com>
2026-07-01 19:06:24 -0500

    agregando el interface usuario.

--- ResidenciaAJ-2026/src/java/Interface/IUsuario.java
+++ ResidenciaAJ-2026/src/java/Interface/IUsuario.java
@@ -1,19 +1,11 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
- */
 package Interface;
 
 import Model.Usuario;
 import java.util.List;
 
-/**
- *
- * @author cielo
- */
 public interface IUsuario {
     List<Usuario> lista();
-    Usuario validate(String username, String password);
+    Usuario validate(String email, String password);
     int insert(Usuario u);
     boolean update(Usuario u);
     Usuario searchById(int id);
