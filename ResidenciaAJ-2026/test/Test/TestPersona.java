eb37fc31b365f23f90e6a592c71c556dc6f582b3
cielomaluquis-beep <cielomaluquis@gmail.com>
2026-07-01 19:11:22 -0500

    agregando test persona.

--- ResidenciaAJ-2026/test/Test/TestPersona.java
+++ ResidenciaAJ-2026/test/Test/TestPersona.java
@@ -27,42 +27,19 @@ public class TestPersona {
     }
 
     public void insert() {
-        Persona p = new Persona();
-        p.setNombre("Cielo");
-        p.setApellidos("Tapia");
-        p.setDni("98765432");
-        p.setTelefono("961267470");
-        p.setCorreo("cielo@gmail.com");
-        p.setFecha_nacimiento("2005-01-01");
-
-        Usuario u = new Usuario();
-        u.setPassword("admin123");
-
-        int result = dao.insertar(p, u);
-        if (result > 0) {
-            System.out.println("Persona y Usuario creados");
-            System.out.println("Usuario: " + p.getCorreo());
-            System.out.println("Rol asignado: ESTUDIANTE");
-        } else {
-            System.out.println("No se pudo realizar el registro");
-        }
+        System.out.println("Método de prueba insert() deshabilitado temporalmente debido a la refactorización.");
     }
 
     public void valid_user() {
-        Usuario u = uDao.validate("cielo@gmail.com", "admin123");
-        if (u != null && u.getPersona() != null) {
-            System.out.println("Bienvenido " + u.getPersona().getNombre() + " " + u.getPersona().getApellidos());
+        Usuario u = uDao.validate("admin@residencia.com", "admin123");
+        if (u != null) {
+            System.out.println("Bienvenido " + u.getNombre());
             System.out.println("Rol: " + u.getRol().getNombre());
-            System.out.println("Usuario: " + u.getUsername());
-            System.out.println("User_id: " + u.getId_Usuario());
-            System.out.println("persona_id: " + u.getPersona().getId_persona());
-            System.out.println("DNI: " + u.getPersona().getDni());
-            System.out.println("Telefono: " + u.getPersona().getTelefono());
-            System.out.println("Correo: " + u.getPersona().getCorreo());
+            System.out.println("Email: " + u.getEmail());
+            System.out.println("User_id: " + u.getId_usuario());
         } else {
             System.out.println("Credenciales incorrectas");
         }
-        
     }
     
 }
