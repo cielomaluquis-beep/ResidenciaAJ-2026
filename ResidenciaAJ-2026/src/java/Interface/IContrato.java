dae9c0e3f52ea58a64a6379892856d30f3bc521d
cielomaluquis-beep <cielomaluquis@gmail.com>
2026-07-01 19:05:47 -0500

    agregando el interface contrato.

--- ResidenciaAJ-2026/src/java/Interface/IContrato.java
+++ ResidenciaAJ-2026/src/java/Interface/IContrato.java
@@ -1,21 +1,13 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
- */
 package Interface;
 
 import Model.Contrato;
 import java.util.List;
 
-/**
- *
- * @author cielo
- */
 public interface IContrato {
-    List<Contrato> lista();
-    int insert(Contrato c);
-    boolean update(Contrato c);
-    Contrato searchById(int id);
-    boolean delete(int id);
-    
+    public int insert(Contrato contrato);
+    public boolean update(Contrato contrato);
+    public boolean delete(int id);
+    public List<Contrato> listar();
+    public Contrato buscar(int id);
+    public boolean actualizarEstado(int id_contrato, String estado);
 }
