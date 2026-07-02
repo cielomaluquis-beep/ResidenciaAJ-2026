3844872fd866956c92d478135a3f591c15433fe7
cielomaluquis-beep <cielomaluquis@gmail.com>
2026-07-01 19:07:07 -0500

    agregando el model contrato.

--- ResidenciaAJ-2026/src/java/Model/Contrato.java
+++ ResidenciaAJ-2026/src/java/Model/Contrato.java
@@ -1,36 +1,24 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package Model;
 
-/**
- *
- * @author cielo
- */
+import java.sql.Date;
+
 public class Contrato {
-    
     private int id_contrato;
-    private int id_persona;
+    private Date fecha_inicio;
+    private Date fecha_fin;
+    private String estado;
+    private int id_tipoContrato;
+    private int id_cliente;
     private int id_habitacion;
-    private String fecha_inicio;
-    private String fecha_fin;
-    private int estado;
-    private Persona persona;
-    private Habitacion habitacion;
+    private int id_usuario;
+
+    // Campos extra para la vista (transient)
+    private String tipoContratoNombre;
+    private String clienteNombreCompleto;
+    private String habitacionNumero;
+    private String usuarioNombre;
 
     public Contrato() {
-    }
-
-    public Contrato(int id_contrato, int id_persona, int id_habitacion, String fecha_inicio, String fecha_fin, int estado, Persona persona, Habitacion habitacion) {
-        this.id_contrato = id_contrato;
-        this.id_persona = id_persona;
-        this.id_habitacion = id_habitacion;
-        this.fecha_inicio = fecha_inicio;
-        this.fecha_fin = fecha_fin;
-        this.estado = estado;
-        this.persona = persona;
-        this.habitacion = habitacion;
     }
 
     public int getId_contrato() {
@@ -41,12 +29,44 @@ public class Contrato {
         this.id_contrato = id_contrato;
     }
 
-    public int getId_persona() {
-        return id_persona;
+    public Date getFecha_inicio() {
+        return fecha_inicio;
     }
 
-    public void setId_persona(int id_persona) {
-        this.id_persona = id_persona;
+    public void setFecha_inicio(Date fecha_inicio) {
+        this.fecha_inicio = fecha_inicio;
+    }
+
+    public Date getFecha_fin() {
+        return fecha_fin;
+    }
+
+    public void setFecha_fin(Date fecha_fin) {
+        this.fecha_fin = fecha_fin;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+
+    public int getId_tipoContrato() {
+        return id_tipoContrato;
+    }
+
+    public void setId_tipoContrato(int id_tipoContrato) {
+        this.id_tipoContrato = id_tipoContrato;
+    }
+
+    public int getId_cliente() {
+        return id_cliente;
+    }
+
+    public void setId_cliente(int id_cliente) {
+        this.id_cliente = id_cliente;
     }
 
     public int getId_habitacion() {
@@ -57,45 +77,44 @@ public class Contrato {
         this.id_habitacion = id_habitacion;
     }
 
-    public String getFecha_inicio() {
-        return fecha_inicio;
+    public int getId_usuario() {
+        return id_usuario;
     }
 
-    public void setFecha_inicio(String fecha_inicio) {
-        this.fecha_inicio = fecha_inicio;
+    public void setId_usuario(int id_usuario) {
+        this.id_usuario = id_usuario;
     }
 
-    public String getFecha_fin() {
-        return fecha_fin;
+    // Getters y Setters Extras
+    public String getTipoContratoNombre() {
+        return tipoContratoNombre;
     }
 
-    public void setFecha_fin(String fecha_fin) {
-        this.fecha_fin = fecha_fin;
+    public void setTipoContratoNombre(String tipoContratoNombre) {
+        this.tipoContratoNombre = tipoContratoNombre;
     }
 
-    public int getEstado() {
-        return estado;
+    public String getClienteNombreCompleto() {
+        return clienteNombreCompleto;
     }
 
-    public void setEstado(int estado) {
-        this.estado = estado;
+    public void setClienteNombreCompleto(String clienteNombreCompleto) {
+        this.clienteNombreCompleto = clienteNombreCompleto;
     }
 
-    public Persona getPersona() {
-        return persona;
+    public String getHabitacionNumero() {
+        return habitacionNumero;
     }
 
-    public void setPersona(Persona persona) {
-        this.persona = persona;
+    public void setHabitacionNumero(String habitacionNumero) {
+        this.habitacionNumero = habitacionNumero;
     }
 
-    public Habitacion getHabitacion() {
-        return habitacion;
+    public String getUsuarioNombre() {
+        return usuarioNombre;
     }
 
-    public void setHabitacion(Habitacion habitacion) {
-        this.habitacion = habitacion;
+    public void setUsuarioNombre(String usuarioNombre) {
+        this.usuarioNombre = usuarioNombre;
     }
-    
-    
 }
